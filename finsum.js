e3fe4fd37a34ad73a1fef4565a5843add24621c4
danenrich <dan@enrichconsulting.com>
2013-06-06 18:05:49 -0700

    Blew threw Rich's exercise 1, but totally stuck on 2.
    The text() and html() functions return a lot of garbage, not just the cell contents. Bad news.

---
+++ finsum.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+    /* exercise 1
+    $("#Financial_summary_c tr:gt(0) td:gt(0)").addClass("interiorCells");
+    $("#Financial_summary_c th").addClass("headers");
+    $("#Financial_summary_c tr td:first-child").addClass("headers");
+    $("#Financial_summary_c td, th").css("padding", "0px");
+    $("#Financial_summary_c .corner").css("visibility", "hidden");
+    */
+    /*
+    var $bigcells = $('#Financial_summary_c td').filter(function () {
+    return parseInt($(this).val().replace("$", ""), 10) > 0;
+    });
+    
+    $("$bigcells").addClass("blueText");
+    $("$bigcells").each(function () {
+    alert("You got me!");
+    });
+    */
+    $('#Financial_summary_c td').filter(function () {
+        //return parseInt($(this).val().replace("$", ""), 10) > 0;
+        return $(this).html === "$20";
+    }).css("color", "blue");
+
+    $('#Financial_summary_c td').each(function () {
+        $(this).click(function () {
+            //alert($(this).html);
+            if ($(this).html === "see me") {
+                var $var = "Yes";
+            }
+            else { var $var = "No"; }
+            $(this).html($var);
+        });
+    });
+    $('#Financial_summary_c td:contains("see me")').each(function () {
+        $(this).css("color", "blue");
+        var $len = $(this).text;
+        $(this).html($len);
+    });
+
+    /*
+    $bigcells.hover(function () {
+    $(this).css("color", "blue");
+    });
+    */
+});
