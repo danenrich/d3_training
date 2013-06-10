bc33c7dee3a7b10cc14249900f778183ca657d8c
danenrich <dan@enrichconsulting.com>
2013-06-10 15:42:02 -0700

    Finish ex 2.

--- finsum.js
+++ finsum.js
@@ -17,9 +17,7 @@
     });
     */
 
-    var $bigcells = $('#Financial_summary_c td').filter(function () {
-        return parseInt($(this).html().replace(/[^0-9\.\(\)-]+/g, ""), 10) > 0;
-    }).css("color", "blue");
+ 
 
     //$('#Financial_summary_c tr:even td:not(:first-child)').css("background-color", "purple");
 
@@ -31,4 +29,32 @@
 
         }
     );
+    //Create total column
+	    //Add total column
+			$('<td>Totals</td>').appendTo('#Financial_summary_c tr:eq(0)');
+	    $('<td></td>').appendTo('#Financial_summary_c tr:gt(0)');
+			
+			//Format all the cells
+			$("#Financial_summary_c td:last-child").addClass(function() {
+				return $(this).prev().attr('class');
+			});
+			
+			//Drop in totals
+			$("#Financial_summary_c tr:gt(0)").each(function() {
+				var sum = 0;
+				//$(this).children(":not(:last-child)").each(function() {
+				$(this).children().not(":first").not(":last").each(function() {
+					var temp = 0;
+					//sum += parseInt($(this).html().replace(/[^0-9\.-]+/g, "0"), 10);
+					temp = parseInt($(this).html().replace(/[^0-9\.-]+/g, ""), 10);
+					//temp = parseInt($(this).html().replace(/[^0-9\.-]+/g, 0),10);
+					sum = isNaN(temp) ? sum : sum+=temp;
+				});
+				sum = sum===0 ? "" : "$" + sum;
+				$(this).children(":last-child").text(sum);
+			});
+		
+		var $bigcells = $('#Financial_summary_c td').filter(function () {
+    	return parseInt($(this).html().replace(/[^0-9\.\(\)-]+/g, ""), 10) > 0;
+    }).css("color", "blue");
 });
