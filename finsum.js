da306a64a0c77907a6d27239652e38dc6bbe1d71
danenrich <dan@enrichconsulting.com>
2013-06-10 17:19:50 -0700

    Exercise 3: Finished creating the second and third tables.

--- finsum.js
+++ finsum.js
@@ -40,12 +40,9 @@
 				//Drop in totals
 				$("#Financial_summary_c tr:gt(0)").each(function() {
 					var sum = 0;
-					//$(this).children(":not(:last-child)").each(function() {
 					$(this).children().not(":first").not(":last").each(function() {
 						var temp = 0;
-						//sum += parseInt($(this).html().replace(/[^0-9\.-]+/g, "0"), 10);
 						temp = parseInt($(this).html().replace(/[^0-9\.-]+/g, ""), 10);
-						//temp = parseInt($(this).html().replace(/[^0-9\.-]+/g, 0),10);
 						sum = isNaN(temp) ? sum : sum+=temp;
 					});
 					sum = sum===0 ? "" : "$" + sum;
@@ -57,7 +54,37 @@
 	   	}).css("color", "blue");
 	  
 	  //Exercise 3
+			//Build the second table
 			$("#Financial_summary_c").parents("div").append($("#Financial_summary_c").clone());			
-			$("table").not("#Financial_summary_c:first").attr("id","ratios");
-			$("#ratios td").css("background-color","red");
+			$("table").not("#Financial_summary_c:first").attr("id","tabletwo");
+			//$("#tabletwo td").css("background-color","red");
+						
+			//Build the ratios table
+			$("#tabletwo").parents("div").append($("#tabletwo").clone());			
+			$("table").not("#tabletwo:first").not("#Financial_summary_c:first").attr("id","ratios");
+			//$("#ratios td").css("background-color","yellow");
+			
+			//Modify the values of the second table by multiplying each cell by its position
+			$("#tabletwo tr:gt(0)").each(function() {
+				$(this).children().not(":first").each(function() {
+					var num = 0;
+					num = parseInt($(this).html().replace(/[^0-9\.-]+/g, ""), 10);
+					num = isNaN(num) ? "" : "$" + (num*$(this).index()).toFixed(0);
+					$(this).text(num);
+				});						
+			});
+
+			//Create the ratios in table three
+			$("#ratios tr:gt(0)").each(function() {
+				$(this).children().not(":first").each(function() {
+					var numerator = 0;
+					var denominator = 0;
+					numerator = parseInt($(this).html().replace(/[^0-9\.-]+/g, ""), 10); //Table 3 has table 1's values, so that's our numerator
+					var rowpos = $(this).parent("tr").index();
+					var colpos = $(this).index();
+					var denominator = parseInt($("#tabletwo tr:eq("+rowpos+") td:eq("+colpos+")").html().replace(/[^0-9\.-]+/g, ""), 10);
+					$(this).text(isNaN(denominator) ? "" : (100*numerator/denominator).toFixed(0) + "%");
+				});						
+			});				
+
 });
